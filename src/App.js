b84a97ce37f41450b17b4350b8eb8d1d755952c5
Roshan Maharjan <40727483+roshanmjn@users.noreply.github.com>
2024-12-19 21:48:58 +0545

    comment lines

--- src/App.js
+++ src/App.js
@@ -20,4 +20,38 @@ const App = () => {
   );
 };
 
+// route for test purpose only.
+// app.post("/write", async (req, res) => {
+//   // console.log({body:req.body});
+//   console.log('msg by:',req.body?.sender?.name);
+//   Logger.info(`writeMessage, ${JSON.stringify(
+//       {
+//           name: req.body.sender?.name,
+//           id: req.body.sender?.id,
+//           message:req.body.message?.text
+//       }
+//   )}`);
+  
+//   const payload = {
+//       "receiver": req.body?.sender?.id,
+//       "min_api_version":1,
+//       "sender":{
+//           "name":`A Quote For ${req.body?.sender?.name}` ?? "Forced Quote",
+//           "avatar":"https://i.pravatar.cc/100"
+//       },
+//       "tracking_data":"tracking data",
+//       "type":"text",
+//       "text": require('../tesData.json')[0].quotes[Math.floor(Math.random() * 41)]
+//   }
+//   if(req.body?.sender?.id && req.body){
+//       console.log({name:req.body.sender.name, id:req.body.sender.id})
+//       const reply = await axios.post("https://chatapi.viber.com/pa/send_message",payload,{
+//           headers: {
+//               'X-Viber-Auth-Token': process.env.MS_CLIENT_ID || 'zzz'
+//           }
+//       });
+//   }
+     
+//   return res.status(200).json({ message: req.body?.message });
+// });
 export default App;
